18fd81f55447a30cc0f6c0f8ea44a44a6dfd125d
zjhmale <zjhmale@gmail.com>
2017-09-30 19:53:40 +0800

    Basic checking elements

--- src/checker.hpp
+++ src/checker.hpp
@@ -1,24 +1,113 @@
 #include <set>
 #include <map>
 #include <string>
+#include <vector>
 #include <memory>
+#include <functional>
+#include <range/v3/all.hpp>
+#ifndef FORMAT_HEADER
+#define FORMAT_HEADER
+#include <fmt/format.h>
+#include <fmt/format.cc>
+#endif
 #include "ast.hpp"
 #include "type.hpp"
 
 using namespace std;
 using namespace ast;
+using namespace fmt;
 using namespace type;
+using namespace ranges;
 
 namespace checker {
-  typedef map<string, shared_ptr<Node>> environment;
-  typedef set<TypeVariable> type_vars;
+  typedef map<string, shared_ptr<Type>> environment;
+  typedef map<shared_ptr<Type>, shared_ptr<Type>> typevar_mapping;
+  typedef set<Type> typevars;
 
   template<typename Base, typename T>
   inline bool instanceof(const shared_ptr<T> ptr) {
     return dynamic_pointer_cast<Base>(ptr) != nullptr;
   }
 
-  auto get_type(string name, environment env, type_vars non_generic) -> shared_ptr<TypeOperator> {
-    return nullptr;
+  auto prune(shared_ptr<Type> t) -> shared_ptr<Type> {
+    if (t->type() == TypeType::VARIABLE) {
+      auto var = static_pointer_cast<TypeVariable>(t);
+      if (var->instance == nullptr) {
+        return t;
+      } else {
+        auto new_instance = prune(var->instance);
+        var->instance = new_instance;
+        return new_instance;
+      }
+    } else {
+      return t;
+    }
+  }
+
+
+  auto occurs_in_type(shared_ptr<Type> var, shared_ptr<Type> t) -> bool;
+
+  auto occurs_in(shared_ptr<Type> var, vector<shared_ptr<Type>> types) -> bool {
+    for (auto iter = types.begin(); iter != types.end(); iter++) {
+      if (occurs_in_type(var, *iter)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  auto occurs_in_type(shared_ptr<Type> var, shared_ptr<Type> t) -> bool {
+    auto pruned = prune(t);
+    if (var == pruned) {
+      return true;
+    } else if (pruned->type() == TypeType::OPERATOR) {
+      return occurs_in(var, static_pointer_cast<TypeOperator>(pruned)->types);
+    } else {
+      return false;
+    }
+  }
+
+  auto is_generic(shared_ptr<Type> var, typevars non_generic) -> bool {
+    vector<shared_ptr<Type>> non_generic_vector = {};
+    std::copy(non_generic.begin(), non_generic.end(), std::back_inserter(non_generic_vector));
+    return !occurs_in(var, non_generic_vector);
+  }
+
+  auto fresh(shared_ptr<Type> t, typevars non_generic) -> shared_ptr<Type> {
+    typevar_mapping mapping = {};
+
+    function<shared_ptr<Type>(shared_ptr<Type>)> fresh_rec = [&](shared_ptr<Type> tp) -> shared_ptr<Type> {
+      auto pruned = prune(tp);
+      if (pruned->type() == TypeType::VARIABLE) {
+        if (is_generic(pruned, non_generic)) {
+          auto result = mapping.find(pruned);
+          if (result != mapping.end()) {
+            mapping[pruned] = make_shared<TypeVariable>();
+          }
+          return mapping[pruned];
+        } else {
+          return pruned;
+        }
+      } else if (pruned->type() == TypeType::OPERATOR) {
+        auto oper = static_pointer_cast<TypeOperator>(pruned);
+        auto freshs = oper->types | view::transform([&](shared_ptr<Type> ta) {
+            return fresh_rec(ta);
+          });
+        return make_shared<TypeOperator>(oper->name, freshs);
+      } else {
+        return nullptr;
+      }
+    };
+
+    return fresh_rec(t);
+  }
+
+  auto get_type(string name, environment env, typevars non_generic) -> shared_ptr<Type> {
+    auto result = env.find(name);
+    if (result != env.end()) {
+      return fresh(env[name], non_generic);
+    } else {
+      throw std::runtime_error(format("Undefined symbol {}", name));
+    }
   }
 }
--- src/type.hpp
+++ src/type.hpp
@@ -22,57 +22,17 @@ namespace type {
   class Type {
   public:
     virtual TypeType type() = 0;
+    virtual string to_string() = 0;
   };
-
-  class TypeOperator : public Type {
-  private:
-    string name;
-    vector<shared_ptr<TypeOperator>> types;
-  public:
-    TypeOperator(string name,
-                 vector<shared_ptr<TypeOperator>> types)
-      : name(name), types(types) {};
-
-    TypeType type() {
-      return TypeType::OPERATOR;
-    }
-
-    virtual string to_string() {
-      if (this->types.size() == 0) {
-        return this->name;
-      } else if (this->types.size() == 2) {
-        return format("{0} {1} {2}", this->types[0]->to_string(), this->name, this->types[2]->to_string());
-      } else {
-        auto literal = this->types
-          | view::transform([](shared_ptr<TypeOperator> t) -> string {
-            return t->to_string();
-          })
-          | view::join(' ');
-        return format("{0} {1}", this->name, literal);
-      }
-    }
-  };
-
-  class FunctionType : public TypeOperator {
-  public:
-    FunctionType(shared_ptr<TypeOperator> from_type,
-                 shared_ptr<TypeOperator> to_type)
-      : TypeOperator("->", vector<shared_ptr<TypeOperator>>({ from_type, to_type })) {};
-  };
-
-  auto IntegerType = make_shared<TypeOperator>("int", vector<shared_ptr<TypeOperator>>({}));
-  auto BooleanType = make_shared<TypeOperator>("bool", vector<shared_ptr<TypeOperator>>({}));
-  auto StringType = make_shared<TypeOperator>("string", vector<shared_ptr<TypeOperator>>({}));
 
   class TypeVariable : public Type {
-  private:
+  public:
     int id;
     char name;
     static int next_id;
     static char next_name;
-    shared_ptr<TypeOperator> instance;
+    shared_ptr<Type> instance;
 
-  public:
     TypeVariable() {
       this->id = TypeVariable::next_id;
       TypeVariable::next_id += 1;
@@ -107,4 +67,65 @@ namespace type {
 
   int TypeVariable::next_id = 0;
   char TypeVariable::next_name = 'a';
+
+  class TypeOperator : public Type {
+  public:
+    string name;
+    vector<shared_ptr<Type>> types;
+
+    TypeOperator(string name,
+                 vector<shared_ptr<Type>> types)
+      : name(name), types(types) {};
+
+    TypeType type() {
+      return TypeType::OPERATOR;
+    }
+
+    string to_string() {
+      if (this->types.size() == 0) {
+        return this->name;
+      } else if (this->types.size() == 2) {
+        return format("{0} {1} {2}", this->types[0]->to_string(), this->name, this->types[2]->to_string());
+      } else {
+        auto literal = this->types
+          | view::transform([](shared_ptr<Type> t) -> string {
+            return t->to_string();
+          })
+          | view::join(' ');
+        return format("{0} {1}", this->name, literal);
+      }
+    }
+  };
+
+  auto IntegerType = make_shared<TypeOperator>("int", vector<shared_ptr<TypeOperator>>({}));
+  auto BooleanType = make_shared<TypeOperator>("bool", vector<shared_ptr<TypeOperator>>({}));
+  auto StringType = make_shared<TypeOperator>("string", vector<shared_ptr<TypeOperator>>({}));
+  auto FunctionType(shared_ptr<Type> from_type, shared_ptr<Type> to_type) -> shared_ptr<TypeOperator> {
+    return make_shared<TypeOperator>("->", vector<shared_ptr<Type>>({ from_type, to_type }));
+  }
+
+  bool operator==(shared_ptr<Type> t1, shared_ptr<Type> t2) {
+    if (t1->type() != t2->type()) {
+      return false;
+    } else {
+      if (t1->type() == TypeType::VARIABLE) {
+        auto var1 = static_pointer_cast<TypeVariable>(t1);
+        auto var2 = static_pointer_cast<TypeVariable>(t2);
+
+        return
+          var1->id == var2->id &&
+          var1->name == var2->name &&
+          var1->instance == var2->instance;
+      } else if (t1->type() == TypeType::OPERATOR) {
+        auto oper1 = static_pointer_cast<TypeOperator>(t1);
+        auto oper2 = static_pointer_cast<TypeOperator>(t2);
+
+        return
+          oper1->name == oper2->name &&
+          oper1->types == oper2->types;
+      } else {
+        return false;
+      }
+    }
+  }
 }
